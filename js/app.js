a8ac9abf1d60a8b0c7ea6a735ebdb44ec7eb3e79
oraclemoss <vaughneric4@gmail.com>
2026-08-05 16:15:20 -0500

    Updated import with new Grid.js filename

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { Grid } from "./grid.js";
+import { Grid } from "./Grid.js";
 
 // Create a default 8x8 grid
-const gameGrid = new Grid("grid-container");
+export const puzzelGrid = new Grid("grid-container");
