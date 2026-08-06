f854e59a0f71a8afb18b41af297051272530fd8d
oraclemoss <vaughneric4@gmail.com>
2026-08-06 18:35:19 -0500

    Reorginized top of file's lines

--- js/ResetButton.js
+++ js/ResetButton.js
@@ -1,7 +1,7 @@
-const resetButton = document.getElementById("reset-button");
-
 // Import the shared container from grid.js
 import { puzzleGrid } from "./app.js";
+
+const resetButton = document.getElementById("reset-button");
 
 resetButton.addEventListener("click", () => {
     console.log("Reset button clicked!");
