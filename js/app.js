dd8456294a3046a7bd75531bdab6e6e2c1de104a
oraclemoss <vaughneric4@gmail.com>
2026-08-06 18:31:15 -0500

    Fixed spelling mistake

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 import { Grid } from "./Grid.js";
 
 // Create a default-sized (8x8) grid
-export const puzzelGrid = new Grid("grid-container");
+export const puzzleGrid = new Grid("grid-container");
