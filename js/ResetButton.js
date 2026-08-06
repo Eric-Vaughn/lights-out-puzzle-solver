4af500f6821586b659d095099c6b0b733fd80fce
oraclemoss <vaughneric4@gmail.com>
2026-08-06 18:33:41 -0500

    Move ResetButton.js's functionality into a Grid.js class method. ResetButton simply calls that method now

--- js/ResetButton.js
+++ js/ResetButton.js
@@ -1,19 +1,9 @@
 const resetButton = document.getElementById("reset-button");
 
 // Import the shared container from grid.js
-import { puzzelGrid } from "./app.js";
+import { puzzleGrid } from "./app.js";
 
 resetButton.addEventListener("click", () => {
     console.log("Reset button clicked!");
-    // Find only the cells that are currently "on"
-    const activeCells = puzzelGrid.gridContainer.querySelectorAll('.cell[data-status="on"]');
-    console.log(`Found ${activeCells.length} active cells to reset.`);
-
-    // Loop through them and change their status back to "off"
-    activeCells.forEach(cell => {
-        cell.dataset.status = "off";
-        
-        // Remove the active CSS class(es) that is styling them
-        cell.classList.remove("on"); 
-    });
+    puzzleGrid.turnAllCellsOff();
 });
